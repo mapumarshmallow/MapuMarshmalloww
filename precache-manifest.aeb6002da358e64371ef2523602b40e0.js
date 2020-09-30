1009761d82f8b06dea846d981dfaaecdee50d047
Hoang Luu <hoang.luu8@mavs.uta.edu>
2020-09-30 16:46:56 -0500

    Updates

--- precache-manifest.aeb6002da358e64371ef2523602b40e0.js
+++ precache-manifest.aeb6002da358e64371ef2523602b40e0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60b2d76dc2bf72ed60d6e8b13a7728d0",
+    "revision": "5c6c384a774b081d39df28901c986cfb",
     "url": "/MapuMarshmalloww/index.html"
   },
   {
-    "revision": "bb1f900643457cafb1bf",
+    "revision": "4acc34930ac4ab09f586",
     "url": "/MapuMarshmalloww/static/css/main.b5696f6a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MapuMarshmalloww/static/js/2.030faea8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb1f900643457cafb1bf",
-    "url": "/MapuMarshmalloww/static/js/main.7dcf67dd.chunk.js"
+    "revision": "4acc34930ac4ab09f586",
+    "url": "/MapuMarshmalloww/static/js/main.8ca7500a.chunk.js"
   },
   {
     "revision": "15b61f57118361fe061c",
